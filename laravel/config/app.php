4b8b5b465502864d5be9dc88dd3570ad8af7bf11
Hugo Júnior <contato@hugojunior.com>
2023-11-02 14:55:56 -0300

    Added Bugsnag tracking

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
         /*
          * Application Service Providers...
          */
