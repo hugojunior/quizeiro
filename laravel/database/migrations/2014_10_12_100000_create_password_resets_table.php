f975a6c1ebe752975aef46a46f66f7e1882494ec
Hugo Júnior <contato@hugojunior.com>
2023-11-02 12:50:11 -0300

    Added SQL migrate rule

--- laravel/database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ laravel/database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,6 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
+        DB::statement('SET SESSION sql_require_primary_key=0');
+
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token');
